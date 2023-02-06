54a98f71e97d10a4b0c0e8fbb5af33c4415e16fc
gueckmooh <gueckmooh@pm.me>
2023-02-06 16:03:49 +0100

    Add -p4diff

--- config/tools/p4-config.el
+++ config/tools/p4-config.el
@@ -1,3 +1,4 @@
+
 ;; =============================================================================
 ;; Perforce configuration
 ;; =============================================================================
@@ -19,11 +20,20 @@
    p4-opened))
 
 ;;;###autoload
+(defun command-line-p4diff (switch)
+  "Use emacs as a p4 diff utility. SWITCH is unused."
+  (let ((file1 (pop command-line-args-left))
+        (file2 (pop command-line-args-left)))
+    (ediff file1 file2)))
+
+;;;###autoload
 (defun command-line-p4merge (switch)
-  "Use emacs as p4 merge utility for perforce. SWITCH is unused."
+  "Use emacs as p4 merge utility. SWITCH is unused."
   (let ((base (pop command-line-args-left))
         (sccs (pop command-line-args-left))
         (mine (pop command-line-args-left))
         (merg (pop command-line-args-left)))
     (ediff-merge-with-ancestor sccs mine base () merg)))
+
+(add-to-list 'command-switch-alist '("-p4diff" . command-line-p4diff))
 (add-to-list 'command-switch-alist '("-p4merge" . command-line-p4merge))
