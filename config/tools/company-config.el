218360f1d077e080ffe4c06b9b807c636fec248f
gueckmooh <gueckmooh@pm.me>
2023-02-08 15:50:15 +0100

    Add recentf mode

--- config/tools/company-config.el
+++ config/tools/company-config.el
@@ -10,7 +10,10 @@
   :ensure t
   :commands (company-mode company-complete-common)
   :bind ("M-i" . company-complete-common)
-  :hook (prog-mode . company-mode)
+  :hook
+  (prog-mode . company-mode)
+  ;; Use recentf mode when using company mode
+  (prog-mode . recentf-mode)
   :config
   (setq-default company-idle-delay              0
                 company-minimum-prefix-length   3
