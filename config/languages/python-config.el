c379b2edd391f5f917af6e2dcac8dfd6bf3e317a
gueckmooh <gueckmooh@pm.me>
2023-02-08 15:44:40 +0100

    Configure pylsp

--- config/languages/python-config.el
+++ config/languages/python-config.el
@@ -16,3 +16,8 @@
 
 (setq-default python-shell-interpreter "/usr/bin/python")
 (setq-default python-indent-offset 4)
+
+(with-eval-after-load 'lsp-config
+  (setq-default lsp-pylsp-plugins-pydocstyle-enabled nil)
+  (setq-default lsp-pylsp-plugins-pycodestyle-max-line-length 81)
+  (setq-default lsp-pyls-plugins-flake8-max-line-length 81))
