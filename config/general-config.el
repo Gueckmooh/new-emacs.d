8549874207f8dcb3eeb9ed007e08bbc1e84c320b
gueckmooh <gueckmooh@pm.me>
2023-02-16 09:22:09 +0100

    Auto revert mode

--- config/general-config.el
+++ config/general-config.el
@@ -258,6 +258,9 @@ point reaches the beginning or end of the buffer, stop there."
 (global-set-key [remap move-beginning-of-line]
                 'smarter-move-beginning-of-line)
 
+;; Toggle reverting buffer when the file changes.
+(global-auto-revert-mode)
+
 ;; =============================================================================
 ;; Utilities
 ;; =============================================================================
