d48aa6960700bcf1a7174f42bd9d123ad439597b
gueckmooh <gueckmooh@pm.me>
2023-02-20 14:13:38 +0100

    Update python config

--- config/languages/python-config.el
+++ config/languages/python-config.el
@@ -10,9 +10,10 @@
 (use-package pipenv
   :hook (python-mode . pipenv-mode)
   :init
-  (setq
-   pipenv-projectile-after-switch-function
-   #'pipenv-projectile-after-switch-extended))
+  ;; (setq
+  ;;  pipenv-projectile-after-switch-function
+  ;;  #'pipenv-projectile-after-switch-extended)
+  )
 
 (setq-default python-shell-interpreter "/usr/bin/python")
 (setq-default python-indent-offset 4)
@@ -20,4 +21,4 @@
 (with-eval-after-load 'lsp-config
   (setq-default lsp-pylsp-plugins-pydocstyle-enabled nil)
   (setq-default lsp-pylsp-plugins-pycodestyle-max-line-length 81)
-  (setq-default lsp-pyls-plugins-flake8-max-line-length 81))
+  (setq-default lsp-pylsp-plugins-flake8-max-line-length 81))
